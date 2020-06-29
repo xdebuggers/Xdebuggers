0d03830a0a40ce2ac358470fab659d8bcf777654
Muhammed Bedavi <mhd.b96@gmail.com>
2020-06-29 11:57:32 +0300

    assets fixed

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" href="{{ asset('images/logo-ico.png') }}" type="image/icon type" />
+    <link rel="icon" href="{{ '/images/logo-ico.png' }}" type="image/icon type" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
+    <link rel="stylesheet" href="{{'/css/style.css' }}" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script
       src="https://kit.fontawesome.com/d8e60bc0b5.js"
@@ -17,7 +17,7 @@
 <body class="app">
     @include('sweetalert::alert')
     @yield('content')
-    <script type="text/javascript" src="{{ asset('js/vendor.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('js/bundle.js') }}"></script>
+    <script type="text/javascript" src="{{ '/js/vendor.js' }}"></script>
+    <script type="text/javascript" src="{{ '/js/bundle.js' }}"></script>
 </body>
 </html>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link rel="icon" href="{{ asset('images/logo-ico.png') }}" type="image/icon type" />
+    <link rel="icon" href="{{ '/images/logo-ico.png' }}" type="image/icon type" />
     <link
       href="https://fonts.googleapis.com/css?family=Raleway&display=swap"
       rel="stylesheet"
     />
 
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+    <link rel="stylesheet" href="{{ '/css/app.css' }}" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script
       src="https://kit.fontawesome.com/d8e60bc0b5.js"
